8f3b5f16788f4c61d6ecfcefc3bf675a6e940f6e
Eric Hunter <ehunter7@live.com>
2021-03-24 14:10:50 -0700

    create nav bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand">Navbar</a>
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
